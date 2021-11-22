19b62a2e415c612a882e87530d70044632194d2d
ugurgpektas <64842537+ugurgpektas@users.noreply.github.com>
2021-11-22 19:40:11 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/widgets_latest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widgets_latest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown,
+      appBar: AppBar(elevation: 20,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)),
+          backgroundColor: Colors.redAccent,
+          centerTitle: true,
+          actions: <Widget>[
+          IconButton(onPressed: null, icon: Icon(Icons.add_circle),
+      alignment: Alignment.centerRight,),
+    ],
+    title: const Text(
+    "Widget Çlaışma sayfası",
+    style: TextStyle(
+    color: Colors.amberAccent,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    )
+    ,
+    )
+    )
+    ,
+    );
+  }
+}
